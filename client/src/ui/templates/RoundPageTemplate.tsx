dea158be82c693fa70d01ca894c91dfdfec8dd91
Richard Ng <richardcrng@gmail.com>
2023-01-07 11:41:18 +0000

    Add action buttons to RoundPageTemplate

--- client/src/ui/templates/RoundPageTemplate.tsx
+++ client/src/ui/templates/RoundPageTemplate.tsx
@@ -4,13 +4,15 @@ import { RoundPrompt } from "../../types/round.types";
 interface Props {
   prompt: RoundPrompt;
   message: string;
+  action?: React.FC<{ style: React.CSSProperties }>;
 }
 
-export default function RoundPageTemplate({ message, prompt }: Props): JSX.Element {
+export default function RoundPageTemplate({ message, prompt, action: Action }: Props): JSX.Element {
   return (
     <Container>
         <QuestionText>{prompt.text}</QuestionText>
         <Message>{message}</Message>
+        {Action && <Action style={{ gridArea: 'action' }} />}
     </Container>
   );
 }
@@ -19,7 +21,8 @@ const Container = styled.div`
   grid-template-areas:
     "question-text"
     "message"
-    "player-data";
+    "player-data"
+    "action";
 `
 
 const QuestionText = styled.h1.attrs({
--- client/src/ui/templates/RoundQuestionApprovalTemplate.tsx
+++ client/src/ui/templates/RoundQuestionApprovalTemplate.tsx
@@ -15,6 +15,15 @@ export default function RoundQuestionApprovalTemplate({ game, player }: Props): 
     <RoundPageTemplate
       prompt={game.round.ongoing.prompt}
       message={message}
+      action={player.isHost ? ({ style }) => (
+        <div
+          className='w-full btn-group btn-group-horizontal'
+          style={style}
+        >
+          <button className='btn btn-error w-1/2'>Draw new</button>
+          <button className='btn btn-success w-1/2'>Approve</button>
+        </div>
+      ) : undefined}
     />
   )
 }
